09630c11737a9304bd942209b1f509041a3ab469
quynhvu793 <vuphannhuquynh@gmail.com>
2025-12-21 11:54:42 +0700

    add testcase

--- src/main/java/org/technical/Database.java
+++ src/main/java/org/technical/Database.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:sqlite:Kindergarten.db";
+    private static final String URL = "jdbc:sqlite:kindergarten.db";
 
     public static Connection getConnection() {
         try {
